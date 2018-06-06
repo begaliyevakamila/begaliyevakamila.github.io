50ff660734ff4fc79d8d652dbd07f691bc5db9cf
begaliyevakamila <kamulesya666>
2018-06-06 07:35:53 +0200

    add bg

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
+const elements = {
+    gridItems: null,
+    pageOverlay: null,
+    aboutBtn: null,
+    aboutSection: null,
+    aboutColumn: null,
+    bookCard: null,
+}
 
+const hideGridItems = () => {
+    elements.gridItems.forEach(item => item.classList.add('kbg-grid-item--hidden'));
+}
 
 const initialize = () => {
-    const gridItems = document.querySelectorAll('.kbg-grid-item');
-    const pageOverlay = document.getElementById('kbg-overlay');
-    const aboutBtn = document.getElementById('kbg-about-btn');
-    const aboutSection = document.getElementById('kbg-about-section');
-    gridItems.forEach(item => {
-        item.addEventListener('click', (e) => {
-            pageOverlay.classList.add('kbg-overlay--showing')
-            setTimeout(() => {
-                const href = item.attributes['data-href'].value;
-                window.location.href = href;
-            }, 500);
-        })
-    });
+    elements.gridItems = document.querySelectorAll('.kbg-grid-item');
+    elements.pageOverlay = document.getElementById('kbg-overlay');
+    elements.aboutBtn = document.getElementById('kbg-about-btn');
+    elements.aboutSection = document.getElementById('kbg-about-section');
+    elements.bookCard = document.getElementById('kbg-book-card');
+    elements.aboutColumn = document.getElementById('kbg-about-col');
+    // elements.gridItems.forEach(item => {
+    //     item.addEventListener('click', (e) => {
+    //         elements.pageOverlay.classList.add('kbg-overlay--showing')
+    //         setTimeout(() => {
+    //             const href = item.attributes['data-href'].value;
+    //             window.location.href = href;
+    //         }, 500);
+    //     })
+    // });
 
-    aboutBtn.addEventListener('click' ,e => {
+    elements.aboutBtn.addEventListener('click', e => {
         e.preventDefault();
-        gridItems.forEach(item => item.classList.add('kbg-grid-item--hidden'));
-        aboutSection.classList.add('kbg-about-section--visible');
+        hideGridItems();
+        elements.aboutSection.classList.add('kbg-about-section--visible');
     })
+
+    elements.gridItems.forEach(item => {
+        item.addEventListener('click', (e) => {
+            hideGridItems();
+            elements.aboutColumn.classList.add('kbg-about-col--hidden');
+            const card = elements[item.attributes['data-card'].value];
+            card.style.display = 'block';
+            card.classList.add('kbg-card--visible');
+        });
+    });
 }
 document.addEventListener("DOMContentLoaded", initialize);
