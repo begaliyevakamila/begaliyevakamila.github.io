7005e30c856a21245965c47ccc9a6fc03a26b3bf
begaliyevakamila <kamulesya666>
2018-06-06 11:21:04 +0200

    more assets

--- script.js
+++ script.js
@@ -10,14 +10,40 @@ const elements = {
 const hideGridItems = () => {
     elements.gridItems.forEach(item => item.classList.add('kbg-grid-item--hidden'));
 }
+const showGridItems = () => {
+    elements.gridItems.forEach(item => item.classList.remove('kbg-grid-item--hidden'));
+}
 
-const initialize = () => {
+const openCard = (item) => {
+    hideGridItems();
+    const card = elements[item.attributes['data-card'].value];
+    const closeBtn = card.querySelector('.kbg-card__close-btn');
+    closeBtn.addEventListener('click', function onCloseClick() {
+        card.classList.remove('kbg-card--visible');
+        card.classList.add('kbg-card--hidden');
+        closeBtn.removeEventListener('click', onCloseClick);
+        card.addEventListener('animationend', function hideCard() {
+            card.style.display = 'none';
+            card.classList.remove('kbg-card--hidden');
+            showGridItems();
+            card.removeEventListener('animationend', hideCard);
+        });
+    });
+    card.style.display = 'block';
+    card.classList.add('kbg-card--visible');
+}
+
+const cacheElements = () => {
     elements.gridItems = document.querySelectorAll('.kbg-grid-item');
     elements.pageOverlay = document.getElementById('kbg-overlay');
     elements.aboutBtn = document.getElementById('kbg-about-btn');
     elements.aboutSection = document.getElementById('kbg-about-section');
     elements.bookCard = document.getElementById('kbg-book-card');
     elements.aboutColumn = document.getElementById('kbg-about-col');
+}
+
+const initialize = () => {
+    cacheElements();
     // elements.gridItems.forEach(item => {
     //     item.addEventListener('click', (e) => {
     //         elements.pageOverlay.classList.add('kbg-overlay--showing')
@@ -28,6 +54,10 @@ const initialize = () => {
     //     })
     // });
 
+    $('.kbg-card__carousel').owlCarousel({
+        items: 1
+    })
+
     elements.aboutBtn.addEventListener('click', e => {
         e.preventDefault();
         hideGridItems();
@@ -35,13 +65,7 @@ const initialize = () => {
     })
 
     elements.gridItems.forEach(item => {
-        item.addEventListener('click', (e) => {
-            hideGridItems();
-            elements.aboutColumn.classList.add('kbg-about-col--hidden');
-            const card = elements[item.attributes['data-card'].value];
-            card.style.display = 'block';
-            card.classList.add('kbg-card--visible');
-        });
+        item.addEventListener('click', e => openCard(item));
     });
 }
 document.addEventListener("DOMContentLoaded", initialize);
